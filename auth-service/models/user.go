68fac1864a53a7c8f7d2386a9fd3b0df5f835957
bilal-os <mb561366@gmail.com>
2025-07-29 15:25:25 +0500

    Removed the password field

--- auth-service/models/user.go
+++ auth-service/models/user.go
@@ -16,7 +16,6 @@ const (
 type User struct {
 	ID        string         `gorm:"primaryKey;type:uuid;default:gen_random_uuid()" json:"id"`
 	Email     string         `gorm:"uniqueIndex;not null" json:"email"`
-	Password  string         `gorm:"not null" json:"-"`
 	Role      UserRole       `gorm:"type:user_role;default:'user'" json:"role"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
